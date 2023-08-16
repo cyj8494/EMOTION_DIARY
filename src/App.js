053030f82206c661606cc8c778a913413567dda5
cyj8494 <cyj8494@gmail.com>
2023-08-17 08:43:18 +0900

    2023-08-17

--- src/App.js
+++ src/App.js
@@ -1,46 +1,131 @@
-import './App.css';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import React, { useReducer, useRef } from "react";
 
-import Home from './pages/Home'
-import New from './pages/New'
-import Edit from './pages/Edit'
-import Diary from './pages/Diary'
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import MyButton from "./components/MyButton";
-import MyHeader from "./components/MyHeader";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Edit from "./pages/Edit";
+import Diary from "./pages/Diary";
+
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      newState = [action.data, ...state];
+      break;
+    }
+    case "REMOVE": {
+      newState = state.filter((it) => it.id !== action.targetId);
+      break;
+    }
+    case "EDIT": {
+      newState = state.map((it) =>
+          it.id === action.data.id ? { ...action.data } : it
+      );
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+};
+
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
+
+const dummyData = [
+  {
+    id: 1,
+    emotion: 1,
+    content: "오늘의일기 1번",
+    date: 1691975068975
+  },
+  {
+    id: 2,
+    emotion: 2,
+    content: "오늘의일기 2번",
+    date: 1691975068976
+  },
+  {
+    id: 3,
+    emotion: 3,
+    content: "오늘의일기 3번",
+    date: 1691975068977
+  },
+  {
+    id: 4,
+    emotion: 4,
+    content: "오늘의일기 4번",
+    date: 1691975068978
+  },
+  {
+    id: 5,
+    emotion: 5,
+    content: "오늘의일기 5번",
+    date: 1691975068979
+  }
+];
 
 function App() {
-  const env = process.env;
-  env.PUBLIC_URL = env.PUBLIC_URL || "";
+  const [data, dispatch] = useReducer(reducer, dummyData);
+
+  const dataId = useRef(0);
+  // CREATE
+  const onCreate = (date, content, emotion) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion
+      }
+    });
+    dataId.current += 1;
+  };
+  // REMOVE
+  const onRemove = (targetId) => {
+    dispatch({ type: "REMOVE", targetId });
+  };
+  // EDIT
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: "EDIT",
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion
+      }
+    });
+  };
 
   return (
-    <BrowserRouter>
-    <div className="App">
-      <MyHeader headText={"App"}
-                leftChild={
-        <MyButton text={"왼쪽 버튼"} onClick={()=> alert("왼쪽 클릭")} />
-      }
-                  rightChild={
-        <MyButton text={"오른쪽 버튼"} onClick={()=> alert("오른쪽 클릭")} />
-      }
-                />
-      <h2>App.js</h2>  {/*// 페이지 전체적으로 보여주려면 routes 바깥에 두기*/}
-
-      <MyButton text={"버튼"} onClick={()=> alert("버튼 클릭")} type={"positive"} />
-      <MyButton text={"버튼"} onClick={()=> alert("버튼 클릭")} type={"negative"} />
-      <MyButton text={"버튼"} onClick={()=> alert("버튼 클릭")} />
-
-      <Routes>
-        <Route path ="/" element={<Home />} />
-        <Route path ="/new" element={<New />} />
-        <Route path ="/edit" element={<Edit />} />
-        <Route path ="/diary/:id" element={<Diary />} />
-      </Routes>
-
-      {/*<a href={"/new"}>NEW로 이동</a>  리액트 라우터에서 a 태그는 외부로 이동할때만*/}
-    </div>
-    </BrowserRouter>
-);
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider
+            value={{
+              onCreate,
+              onEdit,
+              onRemove
+            }}
+        >
+          <BrowserRouter>
+            <div className="App">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/new" element={<New />} />
+                <Route path="/edit" element={<Edit />} />
+                <Route path="/diary/:id" element={<Diary />} />
+              </Routes>
+            </div>
+          </BrowserRouter>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
+  );
 }
 
 export default App;
